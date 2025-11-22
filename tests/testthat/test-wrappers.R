3acaaebff8564c3a372d7da7db672414883b1450
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2025-11-22 15:47:16 -0800

    Changed wrapper test implementation

--- tests/testthat/test-wrappers.R
+++ tests/testthat/test-wrappers.R
@@ -1,86 +1,96 @@
 # Helper to run the comparison loop
 run_wrapper_test <- function(
   measure,
-  wrapper_fn,
-  type = c("default", "binary", "ylp", "ypred")
+  wrapper_fn
 ) {
-  type <- match.arg(type)
+  wrapper_name <- deparse(substitute(wrapper_fn))
 
   test_that(paste(measure, "wrapper matches loo_pred_measure"), {
     for (model_name in names(roaches_models)) {
       inputs <- roaches_models[[model_name]]
       log_weights <- weights(inputs$loo$psis_object)
 
-      if (type == "default") {
-        # mse, rmse, mae, r2
-        expected <- loo_pred_measure(
-          y = inputs$y,
-          mupred = inputs$mupred,
-          loo = inputs$loo,
-          measure = measure
-        )
-        result <- wrapper_fn(
-          y = inputs$y,
-          mupred = inputs$mupred,
-          log_weights = log_weights
-        )
-      } else if (type == "binary") {
-        # acc, balanced_acc
-        expected <- loo_pred_measure(
-          y = inputs$y_binary,
-          mupred = inputs$binary_mupred,
-          loo = inputs$loo,
-          measure = measure
-        )
-        result <- wrapper_fn(
-          y = inputs$y_binary,
-          mupred = inputs$binary_mupred,
-          log_weights = log_weights
-        )
-      } else if (type == "ylp") {
-        # elpd, logscore
-        expected <- loo_pred_measure(
-          y = inputs$y,
-          ylp = inputs$ylp,
-          loo = inputs$loo,
-          measure = measure
-        )
-        result <- wrapper_fn(
-          ylp = inputs$ylp,
-          log_weights = log_weights
-        )
-      } else if (type == "ypred") {
-        # rps, crps, srps, scrps
-        expected <- loo_pred_measure(
-          y = inputs$y,
-          ypred = inputs$ypred,
-          loo = inputs$loo,
-          measure = measure
-        )
-        result <- wrapper_fn(
-          y = inputs$y,
-          ypred = inputs$ypred,
-          log_weights = log_weights
-        )
-      }
+      switch(
+        wrapper_name,
+        mse = ,
+        rmse = ,
+        mae = ,
+        r2 = {
+          expected <- loo_pred_measure(
+            y = inputs$y,
+            mupred = inputs$mupred,
+            loo = inputs$loo,
+            measure = measure
+          )
+          result <- wrapper_fn(
+            y = inputs$y,
+            mupred = inputs$mupred,
+            log_weights = log_weights
+          )
+        },
+        acc = ,
+        balanced_acc = {
+          expected <- loo_pred_measure(
+            y = inputs$y_binary,
+            mupred = inputs$binary_mupred,
+            loo = inputs$loo,
+            measure = measure
+          )
+          result <- wrapper_fn(
+            y = inputs$y_binary,
+            mupred = inputs$binary_mupred,
+            log_weights = log_weights
+          )
+        },
+        elpd = ,
+        logscore = {
+          expected <- loo_pred_measure(
+            y = inputs$y,
+            ylp = inputs$ylp,
+            loo = inputs$loo,
+            measure = measure
+          )
+          result <- wrapper_fn(
+            ylp = inputs$ylp,
+            log_weights = log_weights
+          )
+        },
+        rps = ,
+        crps = ,
+        srps = ,
+        scrps = {
+          expected <- loo_pred_measure(
+            y = inputs$y,
+            ypred = inputs$ypred,
+            loo = inputs$loo,
+            measure = measure
+          )
+          result <- wrapper_fn(
+            y = inputs$y,
+            ypred = inputs$ypred,
+            log_weights = log_weights
+          )
+        },
+        stop("Unknown wrapper: ", wrapper_name)
+      )
 
       expect_equal(result, expected, info = model_name)
     }
   })
 }
 
-run_wrapper_test("mse", mse, "default")
-run_wrapper_test("rmse", rmse, "default")
-run_wrapper_test("mae", mae, "default")
-run_wrapper_test("r2", r2, "default")
+run_wrapper_test("mse", mse)
+run_wrapper_test("rmse", rmse)
+run_wrapper_test("mae", mae)
+run_wrapper_test("r2", r2)
 
-run_wrapper_test("acc", acc, "binary")
-run_wrapper_test("balanced_acc", balanced_acc, "binary")
+run_wrapper_test("acc", acc)
+run_wrapper_test("balanced_acc", balanced_acc)
 
-run_wrapper_test("elpd", elpd, "ylp")
-run_wrapper_test("logscore", logscore, "ylp")
+run_wrapper_test("elpd", elpd)
+run_wrapper_test("logscore", logscore)
 
-run_wrapper_test("rps", rps, "ypred")
-run_wrapper_test("crps", crps, "ypred")
-run_wrapper_test("srps", srps, "ypred")
-run_wrapper_test("scrps", scrps, "ypred")
+run_wrapper_test("rps", rps)
+run_wrapper_test("crps", crps)
+run_wrapper_test("srps", srps)
+run_wrapper_test("scrps", scrps)
